5f4406b295d71b71d8e19509c08568481948d74d
M Kamil Raza <125785123+kamilraza-dev@users.noreply.github.com>
2023-04-07 17:46:18 +0500

    Assignment-01

---
+++ app.js
@@ -0,0 +1,135 @@
+// Q: no 1) Write a JavaScript program that accepts two numbers in two prompts and displays the larger one in the console
+
+
+// let no1 = +prompt("Enter First Number: ")
+// let no2 = +prompt("Enter Second Number: ")
+
+
+// if (no1 > no2) {
+
+//     console.log("First number  " + no1 + " is greater");
+// }
+// else {
+//     console.log("Second number " + no2 + " is greater");
+// }
+
+// -----------------------------------------------------------------------------------------
+
+// Q: no 2) Write a JavaScript conditional statement to find the sign of a number. Display an alert box with the specified sign
+
+
+// let num = +prompt("Enter an Integer : ")
+
+// if (num > 0) {
+//     console.log("The sign is +");
+// }
+// else {
+//     console.log("The sign is -");
+// }
+
+
+// -----------------------------------------------------------------------------------------
+
+// Q: no 3) Write a JavaScript program that accepts five numbers in five prompts and displays the larger one in the console
+
+// let no1 = +prompt("Enter First Number: ")
+// let no2 = +prompt("Enter Second Number: ")
+// let no3 = +prompt("Enter Third Number: ")
+// let no4 = +prompt("Enter Fourth Number: ")
+// let no5 = +prompt("Enter Fifth Number: ")
+
+// if (no1 > no2 && no1 > no3 && no1 > no4 && no1 > no5) {
+//     console.log(no1 + " is Greater Number");
+// }
+// else if (no2 > no3 && no2 > no4 && no2 > no5) {
+//     console.log(no2 + " is Greater Number");
+// } else if (no3 > no4 && no3 > no5) {
+//     console.log(n3 + " is Greater Number");
+// } else if (no4 > no5) {
+//     console.log(no4 + " is Greater Number");
+// } else {
+//     console.log(no5 + " is Greater Number");
+// }
+
+// -----------------------------------------------------------------------------------------
+
+// Q: no 4) Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen
+
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+
+
+// -----------------------------------------------------------------------------------------
+
+// Q: no 5) Write a JavaScript program which computes the average marks of the following students Then, this average is used to determine the corresponding grade.
+
+// let marks = +prompt("Enter Your Marks : ")
+// let grade;
+
+// if (marks >= 90 && marks < 100) {
+//     grade = "A";
+// }
+// else if (marks >= 80 && marks < 90) {
+//     grade = "B";
+// }
+// else if (marks >= 70 && marks < 80) {
+//     grade = "C";
+// }
+// else if (marks >= 60 && marks < 70) {
+//     grade = "D";
+// }
+// else if (marks < 60) {
+//     grade = "F";
+// }
+
+
+// console.log("Your Obtained " + marks + " marks");
+// console.log("Your Grade is " + grade);
+
+
+// --------------------------------------------------------------------------------------------
+
+// Q: no 6) Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+
+// for (let i = 1; i <= 100; i++) {
+
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// --------------------------------------------------------------------------------------------
+
+// Write a JavaScript program to construct the following pattern, using a nested
+// for loop.
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// for (let i = 0; i <= 5; i++) {
+//     for (let j = 0; j < i; j++) {
+//         document.write("*");
+//         console.log("*");
+//     }
+//     console.log("\n");
+//     document.write("<br>");
+// }
+
+
+
+
+
